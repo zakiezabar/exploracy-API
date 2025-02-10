393be5f0dddf431992e828d42f29debc2798bd9a
zakiezabar <zakiezabar@gmail.com>
2025-02-10 10:10:25 +0800

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkLoaded, ClerkLoading, ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
 
 const spaceMono = Space_Mono({
